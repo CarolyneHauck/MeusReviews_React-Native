c78dfc606d051b03db0f6fc3b07faa38548ca5a4
CarolyneHauck <carolyne.carreira@gmail.com>
2020-11-07 21:39:56 -0300

    Add imagens and lottie-react-native

--- firebase.js
+++ firebase.js
@@ -13,4 +13,8 @@ var firebaseConfig = {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
+  if (!firebase.apps.length){
+    firebase.initializeApp(firebaseConfig);
+}
+
   export default firebase
